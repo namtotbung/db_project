d928bded776dc3e4d62a0472a7f51ac8cffcf1c6
namtotbung <nam.nn265@gmail.com>
2022-12-21 15:13:50 +0700

    feature: using pg-promise in db_config.js

--- database/db_config.js
+++ database/db_config.js
@@ -4,3 +4,17 @@ const { Pool } = require("pg");
 const pool = new Pool();
 
 module.exports = { pool };
+
+const pgp = require('pg-promise')();
+
+const cn = {
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD
+};
+
+const db = pgp(cn); // database instance;
+
+module.exports = { db };

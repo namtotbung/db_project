d548f39b0fd469b356aafde9cc854343e51df1d6
namtotbung <nam.nn265@gmail.com>
2023-01-09 18:30:05 +0700

    add: 'clear.sql'

---
+++ sql/clear.sql
@@ -0,0 +1,5 @@
+drop table if exists team cascade;
+drop table if exists people cascade;
+drop table if exists project cascade;
+drop table if exists task cascade;
+drop table if exists account cascade;
